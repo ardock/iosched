7fc90d85f0607297fab8a217ecea04794b0bd25c
Ardock <ardock@gmail.com>
2014-11-11 12:59:42 +0100

    Test all the posibilities

--- scripts/wait_for_emulator.sh
+++ scripts/wait_for_emulator.sh
@@ -16,6 +16,8 @@ until [[ "$bootanim" =~ "stopped" ]]; do
 done
 echo "Done"
 
+
+  # TODO: Optimize wait-for-emulator.sh script. Added "device " to "not found" as a quick fix.
   # Check android device bridge, adb: http://developer.android.com/tools/help/adb.html
   # adb is a versatile command line tool that lets you communicate with an emulator.
   # adb -e: direct an adb command to the only running emulator. Return an error if more than one.
@@ -38,26 +40,21 @@ echo "Done"
   # - offline: instance is not connected to adb or is not responding.
   # - device: instance is connected to the adb server but not implies fully booted and operational.
 
-  # Check command summary: http://developer.android.com/tools/help/adb.html#commandsummary
-  # adb wait-for-device: blocks execution until the device is online (instance state is device).
-  # adb start-server: checks whether the adb server process is running and starts it, if not.
-  # adb stop-server: terminates the adb server process.
-  # Check direct commands: http://developer.android.com/tools/help/adb.html#directingcommands
-  # adb -s <serialNumber> <command>: send an adb command directly to a target when more than one.
-
   # Error states and what to do (based on values returned by init.svc.bootanim property):
   # - device not found: wait until emulator has been created and started or timeout reached.
   # - device offline: wait until emulator connected to adb or timeout reached,
   # - running: device is booting, connected to adb (device state). You can query it about state.
   # - stopped: ui appeared, it's safe to assume that emulator is running and ready to be used.
 
+  #
 
+  # TODO: Test new default. I think need fixes. Create a new script or oneline .travis.yml command.
   # Check new default/android-wait-for-emulator: https://github.com/travis-ci/travis-cookbooks ...
-  # TODO: Test new script. I think need fixes. Create my own script or oneline .travis.yml command.
-  # See issue: https://github.com/travis-ci/travis-ci/issues/2932
+  # See issue,new default needs be fixed: https://github.com/travis-ci/travis-ci/issues/2932
+  # See Fix 1: https://github.com/travis-ci/travis-cookbooks/pull/396
   # UPDATE: They solved the main problem but need optimize the loop without increase the failcounter
-  # See Fix1: https://github.com/travis-ci/travis-cookbooks/pull/396
-  # See Fix2: https://github.com/travis-ci/travis-cookbooks/pull/397
+  # See issue, emulator loading slow: https://code.google.com/p/android/issues/detail?id=77283
+  # See Fix 2: https://github.com/travis-ci/travis-cookbooks/pull/397
 
   # Check  http://stackoverflow.com/questions/19622198/what-does-set-e-in-a-bash-script-mean
   # Type 'help set' in a terminal. Note: '-e' refers to bash usage no to adb -e parameter.
@@ -68,6 +65,7 @@ echo "Done"
   # &: runs a command in the background, You can type other command while background job is running.
   # adb -e shell getprop init.svc.bootanim 2>&1 &: checks in the background if boot anim is running.
 
+  #
 
   # TODO: Support more than one emulator and test mobile and wearable together on travis servers.
   # Check http://developer.android.com/tools/help/adb.html#directingcommands
@@ -77,4 +75,9 @@ echo "Done"
   # See work in progress, renaming AVD: https://code.google.com/p/android/issues/detail?id=78677
   # See work in progress, renaming AVD: https://android-review.googlesource.com/#/c/113019/
 
-
+  # Check command summary: http://developer.android.com/tools/help/adb.html#commandsummary
+  # adb wait-for-device: blocks execution until the device is online (instance state is device).
+  # adb start-server: checks whether the adb server process is running and starts it, if not.
+  # adb stop-server: terminates the adb server process.
+  # Check direct commands: http://developer.android.com/tools/help/adb.html#directingcommands
+  # adb -s <serialNumber> <command>: send an adb command directly to a target when more than one.
